1e5a5727f332f60f4b833cf0c05e6805efcb65ea
TJ <chentianji@qq.com>
2018-01-12 22:28:07 +0800

    init

---
+++ build/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+
+module.exports = {
+	entry : {
+		app : path.join(__dirname, '../client/app.js')
+	},
+	output : {
+		filename : '[name].[hash].js',
+		path : path.join(__dirname, '../dist'),
+		publicPath : ''
+	},
+	module : {
+		rules : [
+			{
+				test : /.jsx$/,
+				loader : 'babel-loader'
+			}, {
+				test : /.js$/,
+				loader : 'babel-loader',
+				exclude: [
+					path.join(__dirname, '../node_modules')
+				]
+			}
+		]
+	}
+};
