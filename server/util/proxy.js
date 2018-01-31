c90a5b57aad8c2a18bb7d727d8b8c6762a2a9e10
TJ <chentianji@qq.com>
2018-01-31 21:37:24 +0800

    code improvement

--- server/util/proxy.js
+++ server/util/proxy.js
@@ -1,15 +1,13 @@
-const router = require('express').Router()
+// const router = require('express').Router()
 const axios = require('axios')
 const querystring = require('query-string')
-
-
 const baseUrl = 'https://cnodejs.org/api/v1'
 
 module.exports = (req, res, next) => {
   const path = req.path
   const user = req.session.user || {}
   const needAccessToken = req.query.needAccessToken
-  console.log(needAccessToken, user.accessToken)
+
   if (needAccessToken && !user.accessToken) {
     res.status(401).send({
       success: false,
@@ -20,13 +18,11 @@ module.exports = (req, res, next) => {
   const query = Object.assign({}, req.query, {
     accesstoken: (needAccessToken && req.method === 'GET') ? user.accessToken : ''
   })
-  if(query.needAccessToken) delete query.needAccessToken
-
-  console.log(req.method)
+  if (query.needAccessToken) delete query.needAccessToken
   axios(`${baseUrl}${path}`, {
     method: req.method,
     params: req.query,
-    data: querystring.stringify( Object.assign({}, req.body, {
+    data: querystring.stringify(Object.assign({}, req.body, {
       accesstoken: (needAccessToken && req.method === 'POST') ? user.accessToken : ''
     })),
     headers: {
@@ -34,15 +30,14 @@ module.exports = (req, res, next) => {
     }
   }).then(resp => {
     if (resp.status === 200) {
-      console.log('prxy ... > '+resp.data)
       res.send(resp.data)
-    }else {
+    } else {
       res.status(resp.status).send(resp.data)
     }
   }).catch(err => {
     if (err.response) {
       res.status(500).send(err.response.data)
-    }else {
+    } else {
       res.status(500).send({
         success: false,
         msg: 'unknow error'
