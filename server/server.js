e70dc4678a9b01995fa5c0a6f80f9d8b21f0d516
TJ <chentianji@qq.com>
2018-02-01 09:30:10 +0800

    format code

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const ReactSSR = require('react-dom/server')
 const favicon = require('serve-favicon')
 const bodyParser = require('body-parser')
 const session = require('express-session')
@@ -20,7 +19,6 @@ app.use(session({
   secret: 'react cnode class'
 }))
 
-
 const isDev = process.env.NODE_ENV === 'development'
 
 app.use('/api/user', require('./util/handle-login'))
@@ -28,16 +26,16 @@ app.use('/api', require('./util/proxy'))
 
 app.use(favicon(path.join(__dirname, '../favicon.ico')))
 
-if(!isDev) {
-	const template = fs.readFileSync(path.join(__dirname, '../dist/server.ejs'), 'utf8')
-	const serverEntry = require('../dist/server-entry')
-	app.use('/public', express.static(path.join(__dirname, '../dist')))
-	app.get('*', (req, res, next)=> {
+if (!isDev) {
+  const template = fs.readFileSync(path.join(__dirname, '../dist/server.ejs'), 'utf8')
+  const serverEntry = require('../dist/server-entry')
+  app.use('/public', express.static(path.join(__dirname, '../dist')))
+  app.get('*', (req, res, next) => {
     serverRender(serverEntry, template, req, res).catch(next)
-	})
-}else {
-	const devStatic = require('./util/dev-static')
-	devStatic(app)
+  })
+} else {
+  const devStatic = require('./util/dev-static')
+  devStatic(app)
 }
 
 app.use((err, req, res, next) => {
@@ -46,5 +44,5 @@ app.use((err, req, res, next) => {
 })
 
 app.listen(3333, () => {
-	console.log('server run on http://localhost:3333');
+  console.log('server run on http://localhost:3333')
 })
